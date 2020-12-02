904c2ca6ee6e4fb38feeaef57c8f51bce1cbf7fa
Scott Gillum <scott@gillum.co.nz>
2020-12-02 23:17:11 +1300

    more regex madness

--- Day2_a.py
+++ Day2_a.py
@@ -17,12 +17,24 @@ print(len(inputs))
 
 import re
 
-int_range1 = 4
-int_range2 = 8
-var_char = 't'
-var_string = 'ftttttrvts'
+#match = re.match(r"(\d+)-(\d+)", line)
+#int_range1 = int(match.group(1))
+#int_range2 = int(match.group(2))
+#var_char = 't'
+#var_string = 'ftttttrvts'
+
+for line in inputs:
+    pattern = r"(\d+)-(\d+)"
+    match = re.match(pattern, line)
+    str_group1 = match.group(1)
+    str_group2 = match.group(2)
+    int_range1 = int(str_group1)
+    int_range2 = int(str_group2)
+    print(str_group1, type(str_group1), int_range1, type(int_range1))
+
 
 if int_range1 <= var_string.count(var_char) <= int_range2:
     print('pass')
 
-for line in inputs:    print(re.match(r"(\d+)", input.line))
+for line in inputs:    
+    print(re.match(r"(\d+)", line))
