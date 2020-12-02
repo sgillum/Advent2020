e112dc617478fbc632589b88e18ab1bedb77d645
Scott Gillum <scott@gillum.co.nz>
2020-12-02 21:43:37 +1300

    getting variables to work

---
+++ Day2_a.py
@@ -0,0 +1,36 @@
+# __file__ contains this file's filename, e.g. aoc_01_a.py when run from this
+# directory or C:\code\aoc\python\2019\aoc_01_a.py when run from elsewhere.
+# We want to replace the "_a.py" at the end with "_input" to get the correct
+# input file name, e.g. aoc_01_input
+input_file = __file__[:-5] + "_input"
+
+# Open the input file
+with open(input_file) as f:
+    # Read the inputs line by line into variable "inputs"
+    inputs = list(f)
+
+# First steps: output _anything_ to check that things are running as expected
+print('this is fine')
+print(len(inputs))
+ 
+# Example inputs:
+#4-5 t: ftttttrvts
+#7-8 k: kkkkkkkf
+#4-6 k: gqjkkk
+#1-2 t: rttb
+
+int_range1 = "4"
+int_range2 = "5"
+var_char = 't'
+var_string = 'ftttttrvts'
+
+print(var_string.count(var_char))
+
+if 10000 <= 12345 <= 30000:
+    print("pass")
+
+if 5 <= var_string.count(var_char) <= 9:
+   print("pass")
+
+if int_range1 <= var_string.count(var_char) <= int_range2:
+    pass
